2de23bf1d95b92190f0e8a4dc7fc51031126a612
Da-Jin Chu <dajinchu@gmail.com>
2019-04-17 00:19:14 -0400

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Sandbox NEU`,
     description: `Sandbox is a club at Northeastern University that creates custom software applications for researchers.`,
     author: `Sandbox Northeastern University`,
   },
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-styled-components`,
