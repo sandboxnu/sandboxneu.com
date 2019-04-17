0add79e42a24dc8e5ca245d891b91a002f6bd710
Da-Jin Chu <dajinchu@gmail.com>
2019-04-17 01:08:56 -0400

    remove typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,12 +28,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
