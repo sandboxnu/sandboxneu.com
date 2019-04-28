c33645d1b6bed7859ab63434a9aa7662f9f1e9dd
Sarah Witt <witt.s@husky.neu.edu>
2019-04-27 21:31:41 -0400

    Add starting components to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,10 @@
 import React from "react"
 import Img from "gatsby-image"
 import PropTypes from "prop-types"
+import Section from "../styles/Section"
 import styled from "styled-components"
 import SquareLogo from "./squareLogo"
+
 
 const SizedLogo = styled(SquareLogo)`
   height: 40px;
@@ -13,12 +15,64 @@ const GrayBackground = styled.div`
   background: #EAECF0;
 `
 
+const Navigate = styled.div`
+  padding-right: 30%;
+  color: #2a426b;
+`
+
+const Contact = styled.div`
+  color: #2a426b;
+`
+
+const FlexSection = styled(Section)`
+  padding-top: 50px;
+  padding-bottom: 70px;
+  display: flex;
+  justify-content: space-between;
+`
+const FooterInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const SocialSection = styled.div`
+  display: flex;
+`
+
+const SocialInfo = ({ info }) => {
+  return (
+    <StyledSocial>
+      <a href={info.url}>
+        <Img fixed={info.icon.childImageSharp.fixed} />
+      </a>
+    </StyledSocial>
+  )
+}
+
+const StyledSocial = styled.div`
+  margin-right: 20px;
+`
+
 const Footer = ({ pages, email, facebook, linkedin }) => (
   <footer>
     <GrayBackground>
-      <SizedLogo />
-      <Img fixed={facebook.icon.childImageSharp.fixed} />
-      Footer
+      <FlexSection>
+        <SizedLogo />
+        <FooterInfo>
+          <Navigate>
+            <h>NAVIGATE</h>
+            <p>home</p>
+          </Navigate>
+          <Contact>
+            <h>CONTACT</h>
+            <p>sandboxneu@gmail.com</p>
+            <SocialSection>
+              <SocialInfo info={facebook} />
+              <SocialInfo info={linkedin} />
+            </SocialSection>
+          </Contact>
+        </FooterInfo>
+      </FlexSection>
     </GrayBackground>
   </footer>
 )
