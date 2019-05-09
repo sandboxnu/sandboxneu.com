f5f0424d81cd942d50688567258ac72a2d985fed
dajinchu <dajinchu@gmail.com>
2019-05-09 17:48:55 -0400

    includeReferrer

--- src/utils/amplitude.js
+++ src/utils/amplitude.js
@@ -2,7 +2,9 @@ import { useEffect } from "react"
 import a from "amplitude-js/amplitude"
 
 export const amplitudeInit = () => {
-  a.getInstance().init("b03da34a4b82cd670be2217a0d5e0938")
+  a.getInstance().init("b03da34a4b82cd670be2217a0d5e0938", null, {
+    includeReferrer: true,
+  })
 }
 
 export const amplitudeLogEvent = (eventName, props) => {
