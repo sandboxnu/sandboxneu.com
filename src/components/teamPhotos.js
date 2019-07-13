8eecd897a4d320072a95ff409264220e32644789
Sauharda Rajbhandari <sauharda19@gmail.com>
2019-07-14 03:57:06 +0545

    Functioning media links on team page. Not applicable on mobile

--- src/components/teamPhotos.js
+++ src/components/teamPhotos.js
@@ -57,11 +57,26 @@ const ProfileRole = styled.span`
 
 const ProfileImg = styled(Img)`
   border-radius: 50%;
+  transition: all ease-in 150ms;
+`
+
+const ProfileImgWrapper = styled.div`
+  position: relative;
+  width: min-content;
+  margin: auto;
+
+  &:hover .profileImg {
+    filter: blur(2px) grayscale(100%);
+  }
+
+  &:hover .iconWrapper {
+    display: block;
+  }
 `
 
 const ProfileIcon = ({ dest, icon }) => {
   return (
-    <a href={dest}>
+    <a href={dest} target="_blank">
       <StyledIcon icon={icon} color={SB_NAVY} />
     </a>
   )
@@ -69,30 +84,38 @@ const ProfileIcon = ({ dest, icon }) => {
 
 const StyledIcon = styled(FontAwesomeIcon)`
   color: ${props => props.color};
+  font-size: 20px;
   margin: 0rem 0.5rem 0rem 0.5rem;
 `
 
 const IconWrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  display: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  filter: none;
   justify-content: center;
+  transition: all ease-in 2s;
 `
 
 const Profile = ({ member, percent }) => {
-  console.log(member.name)
-  console.log(member.email)
   return (
     <ProfileWrapper percent={percent}>
-      <ProfileImg
-        fixed={member.profileImage.childImageSharp.fixed}
-        alt={member.name}
-      />
+      <ProfileImgWrapper>
+        <ProfileImg
+          className="profileImg"
+          fixed={member.profileImage.childImageSharp.fixed}
+          alt={member.name}
+        />
+        <IconWrapper className="iconWrapper">
+          <ProfileIcon dest={`mailto:${member.email}`} icon={faEnvelope} />
+          <ProfileIcon dest={member.linkedIn} icon={faLinkedinIn} />
+        </IconWrapper>
+      </ProfileImgWrapper>
       <ProfileName>{member.name}</ProfileName>
       <ProfileRole>{member.role}</ProfileRole>
-      <IconWrapper>
-        <ProfileIcon dest={`mailto:${member.email}`} icon={faEnvelope} />
-        <ProfileIcon dest={member.linkedIn} icon={faLinkedinIn} />
-      </IconWrapper>
     </ProfileWrapper>
   )
 }
