de8912b4e07ec23e00a5a0774459b12505d707f2
Ryan Drew <ryan.drew14@gmail.com>
2019-08-19 00:53:27 -0400

    remove unused useMemo call

--- src/templates/apply.js
+++ src/templates/apply.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 import { amplitudeLogEvent } from "utils/amplitude"
