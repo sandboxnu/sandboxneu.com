2a1f25c5c41e5d37febc6e728d195341db90614e
Da-Jin Chu <dchu@alignable.com>
2019-10-06 10:49:23 -0400

    updated orange color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,11 +1,11 @@
 export const SB_LIGHT_BLUE = "#BBE4ED"
 export const SB_NAVY = "#2A426B"
-export const SB_ORANGE = "#FCBC80"
+export const SB_ORANGE = "#FEB729"
 export const SB_YELLOW = "#FEE781"
 
 export const SB_LIGHT_BLUE_RGBA = a => `rgba(187, 228, 237, ${a})`
 export const SB_NAVY_RGBA = a => `rgba(42, 66, 107, ${a})`
-export const SB_ORANGE_RGBA = a => `rgba(252, 188, 128, ${a})`
+export const SB_ORANGE_RGBA = a => `rgba(254, 183, 41, ${a})`
 export const SB_YELLOW_RGBA = a => `rgba(254, 231, 129, ${a})`
 
 // https://css-tricks.com/snippets/javascript/lighten-darken-color/
