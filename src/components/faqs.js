c06e0be9d264c6f3c8a7d780f94b3005991e20d0
cindy1u0 <luo.xiny@husky.neu.edu>
2020-07-20 22:50:26 -0400

    styling changes

--- src/components/faqs.js
+++ src/components/faqs.js
@@ -2,14 +2,35 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import Section from "styles/components/Section"
+import { SB_Light_ORANGE } from "@colors"
 
-const Title = styled.span``
+const Title = styled.div`
+  color: ${SB_Light_ORANGE};
+  font-family: Brandon;
+  font-weight: 600;
+  font-size: 50px;
+  font-style: italic;
+  margin-bottom: 25px;
+`
 
-const Question = styled.span``
+const Question = styled.span`
+  color: #2a426b;
+  font-family: Andale Mono;
+  font-size: 17px;
+  padding-bottom: 14px;
+`
 
-const SingleFAQ = styled.div``
+const SingleFAQ = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 50px;
+`
 
-const Answer = styled.span``
+const Answer = styled.span`
+  color: #1b2432;
+  font-family: Open Sans;
+  font-size: 15px;
+`
 
 const FAQs = ({ faqs }) => (
   <Section>
