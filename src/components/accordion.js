4b19a0503526144cdcdd97aa15713bb8245b32c9
cindy1u0 <luo.xiny@husky.neu.edu>
2020-07-25 15:00:16 -0400

    faq styling

---
+++ src/components/accordion.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { SB_Light_ORANGE, SB_LIGHT_YELLOW } from "@colors"
+
+const Question = styled.span`
+  color: #2a426b;
+  font-family: Andale Mono;
+  font-size: 20px;
+  padding-bottom: 14px;
+`
+
+const Answer = styled.span`
+  color: #1b2432;
+  font-family: Open Sans;
+  font-size: 17px;
+`
+
+const StyledAccordion = styled.div`
+  background-color: ${SB_LIGHT_YELLOW};
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 15px;
+  text-align: left;
+  padding: 18px;
+  width: 100%;
+
+  :hover {
+    background-color: ${SB_Light_ORANGE};
+  }
+`
+
+const StyledPanel = styled.div`
+  padding: 15px 18px;
+  display: ${props => props.display};
+  background-color: white;
+  overflow: hidden;
+`
+
+const Accordion = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <>
+      <StyledAccordion onClick={() => setIsOpen(!isOpen)}>
+        <Question>{question}</Question>
+      </StyledAccordion>
+      <StyledPanel display={isOpen ? "block" : "None"}>
+        <Answer>{answer}</Answer>
+      </StyledPanel>
+    </>
+  )
+}
+
+export default Accordion
--- src/components/faqs.js
+++ src/components/faqs.js
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import Section from "styles/components/Section"
+
 import { SB_Light_ORANGE } from "@colors"
+import Accordion from "./accordion"
 
 const Title = styled.div`
   color: ${SB_Light_ORANGE};
@@ -13,36 +15,16 @@ const Title = styled.div`
   margin-bottom: 25px;
 `
 
-const Question = styled.span`
-  color: #2a426b;
-  font-family: Andale Mono;
-  font-size: 17px;
-  padding-bottom: 14px;
-`
-
-const SingleFAQ = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 50px;
-`
-
-const Answer = styled.span`
-  color: #1b2432;
-  font-family: Open Sans;
-  font-size: 15px;
-`
-
-const FAQs = ({ faqs }) => (
-  <Section>
-    <Title>FAQ</Title>
-    {faqs.map(f => (
-      <SingleFAQ key={f.question}>
-        <Question>{f.question}</Question>
-        <Answer>{f.answer}</Answer>
-      </SingleFAQ>
-    ))}
-  </Section>
-)
+const FAQs = ({ faqs }) => {
+  return (
+    <Section>
+      <Title>FAQ</Title>
+      {faqs.map(f => {
+        return <Accordion question={f.question} answer={f.answer} />
+      })}
+    </Section>
+  )
+}
 
 FAQs.propTypes = {
   faqs: PropTypes.arrayOf(
