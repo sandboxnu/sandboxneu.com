7d75b63f49d64892c1e849c41a83ab313ee60db5
Amiel Monasterial <amielmon@gmail.com>
2020-08-30 15:11:16 -0400

    color config + setup

--- src/styles/components/Banner.js
+++ src/styles/components/Banner.js
@@ -1,22 +1,23 @@
-import styled from "styled-components"
-import { SB_NAVY } from "@colors"
+import tw, { styled, css } from "twin.macro"
 
-const Banner = styled.div`
-  font-family: Montserrat;
-  font-style: italic;
-  font-weight: bold;
-  display: flex;
-  height: 93px;
-  font-size: 25px;
-  line-height: 30px;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-  background: ${SB_NAVY};
-  color: #ffffff;
-  & * {
-    font-family: inherit;
-  }
-`
+const Banner = styled.div([
+  css`
+    font-family: Montserrat;
+    font-style: italic;
+    font-weight: bold;
+    display: flex;
+    height: 93px;
+    font-size: 25px;
+    line-height: 30px;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    ${tw`bg-SB_NAVY`}
+    color: #ffffff;
+    & * {
+      font-family: inherit;
+    }
+  `,
+])
 
 export default Banner
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      SB_NAVY: "#2A426B",
+      SB_ORANGE: "#FEB729",
+      SB_SALMON: "#FA8072",
+      SB_LIGHT_NAVY: "#B2B9C4",
+      SB_LIGHT_ORANGE: "#FFE9C0",
+      SB_LIGHT_SALMON: "#FED9D5",
+      SB_LIGHT_GREY: "#EAECF0",
+    },
+  },
+  variants: {},
+  plugins: [],
+}
