0f215bd20626bbb53141d441842750b4ad66bb4c
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-11-22 13:35:59 -0500

    chore: update cycle to sp25

--- src/pages/about.js
+++ src/pages/about.js
@@ -56,7 +56,7 @@ const AboutPage = ({ data }) => {
       <Banner>
         <JoinBannerContent>
           <div>JOIN OUR TEAM</div>
-          <span>Applications for Fall 2024 are now closed</span>
+          <span>Applications for Spring 2025 are now open!</span>
         </JoinBannerContent>
       </Banner>
     </Layout>
