e4d0adfaaf4ca9014db112983c5ed188a5ff3761
Gavin Normand <75314990+gavinnormand@users.noreply.github.com>
2025-11-24 11:15:58 -0500

    Spring 2026 Apps Open! (#235)

--- src/pages/about.js
+++ src/pages/about.js
@@ -56,7 +56,7 @@ const AboutPage = ({ data }) => {
       <Banner>
         <JoinBannerContent>
           <div>JOIN OUR TEAM</div>
-          <span>Applications for Fall 2025 are closed</span>
+          <span>Applications for Spring 2026 are now open!</span>
         </JoinBannerContent>
       </Banner>
     </Layout>
